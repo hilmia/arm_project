86cd47935ee36aff3d1cc7dbfd54524ab265b965
Hilmi A <2ndgenairborne@gmail.com>
2016-06-11 17:20:16 -0600

    Added Documentation
    Program is ready for submission once all other parties have finished documentation and testing

--- graphics.s
+++ graphics.s
@@ -1,10 +1,8 @@
+
 .section .text
 
-
-
-//r0 - start x
-//r1 - start y
-//r2 - address of picture
+//DrawBlock - Draws a 32x32 pixels starting at the passed in x and y coordinates
+//Args: r0 - start x, r1 - start y, r2 - address of picture
 DrawBlock:
   push {r4 - r10, lr}
 
@@ -18,6 +16,7 @@ DrawBlock:
   column_end      .req r6
   row_end         .req r7
 
+  //Moving into non-scratch registers
   mov picture_address,   r2
   mov column_start,   r1
   mov row_start,  r0
@@ -31,7 +30,9 @@ DrawBlock:
   //row_counter = start (ex. row_start = 0, row_counter = 0)
   mov row_counter,    row_start
 
+  //Starts column counting, setting (resets once per column)
   big_draw_block:
+    //Column Compare
     cmp column_counter, column_end
     beq done_draw_block
 
@@ -39,46 +40,49 @@ DrawBlock:
 
     mov row_counter, row_start
   start_draw_block:
+    //Row Compare
     cmp row_counter, row_end
     beq big_draw_block
-
+    //Do Print
     mov r0, row_counter
     mov r1, column_counter
     ldrh r2, [picture_address], #2
     bl DrawPixel
+    //Go Back
     add row_counter, #1
-
     b start_draw_block
 
+  //Uninitializing variables and finishing subroutine
   done_draw_block:
-
     .unreq column_counter
     .unreq row_counter
-
     .unreq row_start
     .unreq column_start
-
     .unreq row_end
     .unreq column_end
-
     .unreq picture_address
-
     pop {r4 - r10, lr}
     mov pc, lr
 
 
 
+
+
 //r0 - address of array
+//r1 - initialize (1), or reprint (0)
+
+//ArrayToScreen - Fills screen based on Game Array
+//Args: r0 - address of array, r1 - init_flag
+//init_flag: 1 - Initialize (print everything), 0 - Reprint
 .globl ArrayToScreen
 ArrayToScreen:
-  push  {r3 - r10, lr}
+  push  {r4 - r10, lr}
 
   current_element  .req r4
   array            .req r5
   counter          .req r6
   row_counter      .req r10
-  column_counter   .req r3
-  current_picture  .req r7
+  column_counter   .req r7
   x                .req r8
   y                .req r9
 
@@ -90,103 +94,119 @@ ArrayToScreen:
   mov y, #0
   mov column_counter, #0
 
-array_column_loop:
+  //Store init flag
+  ldr r0, =i_buffer
+  str r1, [r0]
 
-  cmp column_counter, #20
+//Same Row/Column Loop concept as in DrawBlock
+array_column_loop:
+  cmp column_counter, #20 //20x20 is what is displayed on a turn
   beq done_array
 
   mov row_counter, #0
-  add x, #32
-  mov y, #192
+  add x, #32  //32 = Tile length
+  mov y, #192 // offset by 6 tiles
 
   array_row_loop:
-
-    cmp row_counter, #80
+    //Loop through row
+    cmp row_counter, #80 // 20x4 because word indexed
     addeq column_counter, #1
     beq array_column_loop
+
     //Load element of array
     ldr   current_element, [array, counter]
 
+    //Goes through every known element and stores the "picture" in r2
     cmp current_element, #13
-    ldreq current_picture, =thirteen
+    ldreq r2, =thirteen
     beq print_element
 
     cmp current_element, #14
-    ldreq current_picture, =fourteen
+    ldreq r2, =fourteen
     beq print_element
 
     cmp current_element, #1
-    ldreq current_picture, =one
+    ldreq r2, =one
     beq print_element
 
     cmp current_element, #2
-    ldreq current_picture, =two
+    ldreq r2, =two
     beq print_element
 
     cmp current_element, #5
-    ldreq current_picture, =five
+    ldreq r2, =five
     beq print_element
 
     cmp current_element, #6
-    ldreq current_picture, =six
+    ldreq r2, =six
     beq print_element
 
     cmp current_element, #7
-    ldreq current_picture, =seven
+    ldreq r2, =seven
     beq print_element
 
     cmp current_element, #8
-    ldreq current_picture, =eight
+    ldreq r2, =eight
     beq print_element
 
     cmp current_element, #9
-    ldreq current_picture, =nine
+    ldreq r2, =nine
     beq print_element
 
     cmp current_element, #12
-    ldreq current_picture, =twelve
+    ldreq r2, =twelve
     beq print_element
 
     cmp current_element, #10
-    ldreq current_picture, =ten
+    ldreq r2, =ten
     beq print_element
 
     cmp current_element, #11
-    ldreq current_picture, =eleven
+    ldreq r2, =eleven
     beq print_element
 
 
     cmp current_element, #17
-    ldreq current_picture, =finish
+    ldreq r2, =finish
     beq print_element
 
+    //Fall-Back, or grass
     cmp current_element, #0
-    ldr current_picture, =zero
+    ldr r2, =zero
 
-
-
-  //This section is where the optimizations are going to lie.
-  //We need to check if block is the same and reprint.
-
+  //Section does actual printing of picture
   print_element:
+    //Store picture into buffer
+    ldr r1, =buffer
+    str r2, [r1]
 
-    //First Time Program Runs
-    //cmp column_counter, #
-    //bgt do_element_print
+    //Loads initializing flag
+    ldr r0, =i_buffer
+    ldr r1, [r0]
+    //If 1 initalize print, do not optimize, otherwise optimize.
+    cmp r1, #1
+    beq do_element_print
 
+    //Check if picture is equal to that of the previous row
     mov r0, current_element
     mov r1, array
     bl checkPicture
 
+    //If returns 1 then element @ row = row - 1, do not print
     cmp r0, #1
     beq done_print_element
 
   do_element_print:
+    //Print Block
+    //Load Picture
+    ldr r1, =buffer
+    ldr r2, [r1]
+
     mov r0, y
     mov r1, x
-    mov r2, current_picture
     bl DrawBlock
 
+  //Increment Variables and Loop back
   done_print_element:
     add y, #32
     add counter, #4
@@ -194,54 +214,64 @@ array_column_loop:
 
     b array_row_loop
 
+  //Uninitializing variables, and ending subroutine
   done_array:
-
     .unreq current_element
     .unreq array
     .unreq counter
-    .unreq current_picture
     .unreq x
     .unreq y
     .unreq column_counter
     .unreq row_counter
-    pop   {r3 - r10, lr}
+    pop   {r4 - r10, lr}
     mov   pc, lr
 
+//checkPicture -  Checks element at current position, and compares it to the one
+//                at the row after. If they match, then no need to print, so
+//                return r0 - 1
+//Args: r0 - Current Element, r1 - Game Array,
+//Return: r0 - If to update (0), or not (1).
 checkPicture:
   push {r4 - r10, lr}
+  //Move Variables into APCS compliant registers
   mov r4, r0
   mov r5, r1
+  //Load element @ row + 1
   ldr r6, [r5, #-80]
   cmp r4, r6
+  //Return Value
   moveq r0, #1
   movne r0, #0
+
   pop {r4 - r10, lr}
   mov pc, lr
 
-//r0 - Contains colour to fill screen with
+//FillScreen - Fills the entire screen with the color passed into r0
+//Args: r0 - Colour
 FillScreen:
   push {r4 - r10, lr}
-
+  //Init
   colour .req r10
   row_size .req r6
   column_size .req r7
   row_counter	.req	r4
   column_counter	.req	r5
 
-
+  //Size of Screen
   mov row_size, #768
   mov column_size, #1024
 
   mov colour, r0
-
+  //Offset because of preincremental counter
   mov row_counter, #-1
 
+  //Goes in row major.
   fill_loop_row:
+    //Checks if entire screen is printed, if so we are done
     cmp row_counter, row_size
     bge fill_done_block
 
     mov column_counter, #0
-
     add row_counter, #1
 
 
@@ -249,11 +279,13 @@ FillScreen:
       cmp column_counter, column_size
       bge fill_loop_row
 
+      //Prints pixel
       mov r2, colour
       mov r0, column_counter
       mov r1, row_counter
       bl DrawPixel
 
+      //Increments Variables and loop back
       add column_counter, #1
       b fill_loop_column
 
@@ -268,12 +300,11 @@ FillScreen:
 
 
 
-/* Draw Pixel
+/* Draw Pixel - Provided by CPSC 359 A3 Specs
  *  r0 - x
  *  r1 - y
  *  r2 - color
  */
-
 DrawPixel:
 	push	{r0, r1, r2, r4, lr}
 
@@ -294,3 +325,18 @@ DrawPixel:
   .unreq offset
 	pop		{r0, r1, r2, r4, lr}
 	mov pc, lr
+
+
+
+.section .data
+
+//Small Buffers used in ArrayToScreen (Cannot use more that r4 - r10)
+buffer:
+  .rept 32
+  .byte 0
+  .endr
+
+i_buffer:
+  .rept 32
+  .byte 0
+  .endr
--- main.s
+++ main.s
@@ -1,7 +1,11 @@
 //CPSC 359 - Assignment 3 - RoadFighter
-//Kyle Ostrander 10128524, Carlin Liu 10123584, Hilmi Abou-Saleh 10125373
-//
-
+@  main.s
+@  Kyle OStrander, Carlin Liu, Hilmi Abou-Saleh
+@  Version - 0.4 (FINAL)
+@  June 13 2016
+@
+@  Description -  Main file. Contains game loop, as well as game handling
+@                 subroutines.
 .section    .init
 .globl     _start
 
@@ -20,24 +24,30 @@ main:
 	bl		InitFrameBuffer
 	bl		Init_GPIO
 
-
+  //Screen to Black - Clear Screen
 	ldr		r0, =0x0000
 	bl		FillScreen
 
+
 	bl		Print_Menu_Start
 
+
 	bl		Menu_Controller
-	cmp		r1, #0
+	cmp		r1, #0 // Check if quit was entered.
+
+  //Start Game
   bleq Game_Loop
 
 haltLoop$:
 	b	haltLoop$
 
 
-//Moves Default array elements to GameArray
+//Reset_Array - Moves default (unchanged) elements to GameArray
+//Args: NONE
 Reset_Array:
   push {r4 - r10, lr}
 
+  //Initialize Variables
   element       .req r4
   defaultArray  .req r5
   changedArray  .req r6
@@ -46,38 +56,49 @@ Reset_Array:
   ldr changedArray, =gameArray
   ldr defaultArray, =gameArray_default
 
+
   mov counter, #0
 
+  //Start Loop
   reset_array_loop:
-  ldr r8, =16000
-  cmp counter, r8
-  beq done_reset
+    ldr r8, =16000 //20*200*4 Size of Array
+    cmp counter, r8
+    beq done_reset
 
-  ldr element, [defaultArray, counter]
-  str element, [changedArray, counter]
+    //Load unchanged element and store it into the changed array
+    ldr element, [defaultArray, counter]
+    str element, [changedArray, counter]
 
-  add counter, #4
-  b reset_array_loop
+    //Increment Variables and branch back
+    add counter, #4
+    b reset_array_loop
 
   done_reset:
-  .unreq element
-  .unreq defaultArray
-  .unreq changedArray
-  .unreq counter
-  pop {r4 - r10, lr}
-  mov pc, lr
+    .unreq element
+    .unreq defaultArray
+    .unreq changedArray
+    .unreq counter
+    pop {r4 - r10, lr}
+    mov pc, lr
 
+//Reset_Game - Resets Array, and starts game again
+//Args: NONE
 .globl Reset_Game
 Reset_Game:
   push {lr}
+  //Reset Game
   bl Reset_Array
+  //Screen to Black
   mov r0, #0
   bl FillScreen
+  //Start Game Again
   bl Game_Loop
+
   pop {lr}
   mov pc, lr
 
-
+//Restart_Game - Resets Array, and Goes to start menu
+//Args: NONE
 .globl Restart_Game
 Restart_Game:
   push {lr}
@@ -87,11 +108,13 @@ Restart_Game:
   mov pc, lr
 
 
-
+//Game_Loop - Main Game Function.
+//Args: NONE
 .globl Game_Loop
 Game_Loop:
   push {r4 - r10, lr}
 
+  //Variable Init
   state       .req r4
   counter     .req r5
   temp_state  .req r6
@@ -105,8 +128,9 @@ Game_Loop:
   add temp_state, #40 //Offset to middle of row.
 
   mov counter, #0
-  mov player, #11
+  mov player, #11 //11 corresponds to player value in game array (see: GameLogic.s)
 
+  //Start Game Initialization
   start_screen_init:
   bl Screen_Data_Init
 
@@ -115,12 +139,12 @@ Game_Loop:
   mov random_row_counter, #0
 
   addEnemy:
-    cmp random_row_counter, #200
+    cmp random_row_counter, #200 //200 is length of game
     bge addFuel_Init
     mov r0, random_row_counter
-    mov r1, #10
+    mov r1, #10 //10 corresponds to an enemy unit
     bl  randomGen
-    add random_row_counter, #5
+    add random_row_counter, #5 //Places enemy every 5 places
     b addEnemy
 
 
@@ -128,6 +152,7 @@ Game_Loop:
     mov random_row_counter, #0
   addFuel:
     cmp random_row_counter, #200
+    movge r9, #1
     bge gameState
     mov r0, random_row_counter
     mov r1, #11
@@ -135,30 +160,38 @@ Game_Loop:
     add random_row_counter, #15
     b addFuel
 
-
-
   //Start Loop
   gameState:
     sub state, #80 //Offset by row  (20x4)
 
     //Update Location
+    sub temp_state, #80 //Every time, offset a row
 
-    sub temp_state, #80
-
-    cmp counter, #200
-    beq end_game
+    cmp counter, #200   //Time to finish line is 200 long
+    beq end_game        //If hit, game is won
 
     mov r0, #12
-    str r0, [temp_state]
+    str r0, [temp_state] //Update Player Location
 
-    //Display Information:
-
+    //Display Information
     bl Screen_Data_Print
 
-    //Update Game
-    mov r0, state
-    bl ArrayToScreen
 
+    //Update Game
+    //Getting ready to update game
+    mov r0, state
+    mov r1, #1
+    //If r9 = 1 then this is the first time the game is run, so print EVERYTHING
+    //otherwise r9 = 0 meaning only update what has changed.
+    cmp r9, #1
+    bleq ArrayToScreen
+    mov r9, #0
+    //Update screen with changed only
+    movne r0, state
+    movne r1, #0
+    blne ArrayToScreen
+
+    //This loop waits for input to be read.
     read_loop:
       bl Read_Data
 
@@ -179,16 +212,16 @@ Game_Loop:
       g_dpadleft:
 
         //Check Fuel or Enemy
-        ldr r2, [temp_state, #-84]
-
+        ldr r2, [temp_state, #-84] //Loading (row + 1) - 4
+        //If enemy
         cmp r2, #10
         beq update_game_for_enemy_hit
 
-
+        //If fuel_vehicle
         cmp r2, #11
         bleq Screen_Data_Add
 
-
+        //Replacing Existing Player
         mov r0, #5
         str r0, [temp_state, #84]
         sub temp_state, #4
@@ -196,7 +229,7 @@ Game_Loop:
 
         b update_game
 
-
+      //Similar to above
       g_dpadrightA:
       	ldr	r1, =0xFE7F
         cmp	r0, r1
@@ -208,7 +241,7 @@ Game_Loop:
         bne g_a_button
       g_dpadright:
         //Check Fuel or Enemy
-        ldr r2, [temp_state, #-76]
+        ldr r2, [temp_state, #-76] //Loading (row+1) +1
 
         cmp r2, #10
         beq update_game_for_enemy_hit
@@ -234,7 +267,7 @@ Game_Loop:
         cmp r2, #10 //Enemy Hit
         beq update_game_for_enemy_hit
 
-        cmp r2, #11
+        cmp r2, #11 //Fuel Hit
         bleq Screen_Data_Add
 
         mov r0, #5
@@ -252,25 +285,26 @@ Game_Loop:
         cmp r0, r1
         moveq r0, #5
         streq r0, [temp_state]
-
-        ldreq r0, =0x0
-        bleq FillScreen
         bleq Reset_Game
 
         b read_loop
 
+    //An enemy has been hit
     update_game_for_enemy_hit:
+      //Subtract Life
       mov r0, #1
       bl Screen_Data_Change
 
+      //Subtract 10 fuel
       mov r0, #0
       mov r1, #10
       bl Screen_Data_Change
 
+      //r9 = 11 - player (shift that needs to occur to replace player @ 11)
       rsb r9, player, #11
       mov r0, #12
       lsl r9, #2 //r9 * 4
-      add temp_state, r9
+      add temp_state, r9 //Update shift to state.
       str r0, [temp_state]
 
       b update_player
@@ -278,11 +312,14 @@ Game_Loop:
     update_game:
       //Check collision with Right Wall
       cmp player, #17
+      //Update Life
       movge r0, #1
       blge  Screen_Data_Change
+      //Update Fuel
       mov r0, #0
       mov r1, #10
       blge Screen_Data_Change
+      //Move Player
       movge r0, #12
       subge temp_state, #24
       strge r0, [temp_state]
@@ -293,12 +330,14 @@ Game_Loop:
       cmp player, #4
       //No collision with left or right wall
       bgt finish_update
-
+      //Update Life
       mov r0, #1
       bl  Screen_Data_Change
+      //Update Fuel
       mov r0, #0
       mov r1, #10
       bl Screen_Data_Change
+      //Move Player
       mov r0, #12
       add temp_state, #28
       str r0, [temp_state]
@@ -308,21 +347,23 @@ Game_Loop:
         mov player, #11
 
       finish_update:
-
+        //Fill Lives/Fuel with Black (will be redrawn in a second)
         ldr r0, =0x0
         bl FillScreen_M
+        //Subtract fuel
         mov r0, #0
         mov r1, #1
         bl Screen_Data_Change
+        //One closer to victory and move player
         add counter, #1
-
         b gameState
-
-
+  //GAME IS WON!
   end_game:
+    //Print Message
     bl Game_Win
+    //Get controller input
     bl Game_End_Menu
-
+    //finished
     .unreq state
     .unreq counter
 
@@ -331,10 +372,6 @@ Game_Loop:
     .unreq random_row_counter
     pop {r4 - r10, lr}
     mov pc, lr
-
-
-
-
 
 .section .data
 
